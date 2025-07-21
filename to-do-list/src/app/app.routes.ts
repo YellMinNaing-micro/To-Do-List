c48588d1e41f13df305dcfe1e3608f13acad92c7
Yell Min Naing <117013435+YellMinNaing-micro@users.noreply.github.com>
2025-07-21 14:54:57 +0630

    Update app.routes.ts

--- to-do-list/src/app/app.routes.ts
+++ to-do-list/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
+import { TodoComponent } from "./pages/todo/todo.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { RegisterComponent } from "./pages/register/register.component";
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  // },
+
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'todo', component: TodoComponent },
 ];
