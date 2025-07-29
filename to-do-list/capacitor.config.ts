01e0bbd85a12823b4eadfac14f46cceb32d72042
Yell Min Naing <117013435+YellMinNaing-micro@users.noreply.github.com>
2025-07-29 16:24:45 +0630

    Update capacitor.config.ts

--- to-do-list/capacitor.config.ts
+++ to-do-list/capacitor.config.ts
@@ -7,3 +7,18 @@ const config: CapacitorConfig = {
 };
 
 export default config;
+
+
+// import type { CapacitorConfig } from '@capacitor/cli';
+//
+// const config: CapacitorConfig = {
+//   appId: 'io.ionic.starter',
+//   appName: 'to-do-list',
+//   webDir: 'www',
+//   server: {
+//     url: 'http://192.168.1.18:8100', // <--- Use your IP from the screenshot here
+//     cleartext: true
+//   }
+// };
+//
+// export default config;
