3fa81e18c39b230f51387635ac10d80eedf6167f
Yell Min Naing <117013435+YellMinNaing-micro@users.noreply.github.com>
2025-07-30 15:28:59 +0630

    Update login.component.ts

--- to-do-list/src/app/pages/login/login.component.ts
+++ to-do-list/src/app/pages/login/login.component.ts
@@ -5,34 +5,35 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  standalone: true,
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-  imports: [IonicModule, FormsModule, CommonModule]
+	standalone: true,
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss'],
+	imports: [IonicModule, FormsModule, CommonModule,
+	]
 })
 export class LoginComponent {
-  username = '';
-  password = '';
-  errorMsg = '';
+	username = '';
+	password = '';
+	errorMsg = '';
 
-  private router = inject(Router); // ✅ replace constructor
+	private router = inject(Router); // ✅ replace constructor
 
-  login() {
-    const savedUser = localStorage.getItem('user');
-    const savedPass = localStorage.getItem('pass');
+	login() {
+		const savedUser = localStorage.getItem('user');
+		const savedPass = localStorage.getItem('pass');
 
-    if (
-        (this.username === 'user' && this.password === 'Password@123') ||
-        (this.username === savedUser && this.password === savedPass)
-    ) {
-      this.router.navigateByUrl('/todo');
-    } else {
-      this.errorMsg = 'Invalid credentials';
-    }
-  }
+		if (
+			(this.username === 'user' && this.password === 'Password@123') ||
+			(this.username === savedUser && this.password === savedPass)
+		) {
+			this.router.navigateByUrl('/todo');
+		} else {
+			this.errorMsg = 'Invalid credentials';
+		}
+	}
 
-  goToRegister() {
-    this.router.navigateByUrl('/register');
-  }
+	goToRegister() {
+		this.router.navigateByUrl('/register');
+	}
 }
